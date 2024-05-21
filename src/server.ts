8c2e6a965aa397325ff87feab86648eb12fd07cc
Gabriel <gabrielgapseu@gmail.com>
2024-05-21 18:41:54 -0300

    Feat: add error handler

--- src/server.ts
+++ src/server.ts
@@ -1,16 +1,21 @@
-import express from "express"
-import dotenv from "dotenv"
-import db from "./config/db"
+import express, { Request, Response, NextFunction } from 'express';
+import dotenv from 'dotenv';
+import db from './config/db';
 
-dotenv.config()
+dotenv.config();
 
-const port = process.env.PORT!
-const app = express()
+const port = process.env.PORT!;
+const app = express();
 
-app.use(express.json())
+app.use(express.json());
 
 app.listen(port, async () => {
     console.log('Server running on port 3000');
     await db();
-})
+});
 
+app.use((err: any, req: Request, res: Response, next: NextFunction) => {
+    const statusCode = err.statusCode || 500;
+    const message = err.message || 'Internal Server Error';
+    return res.status(statusCode).json({ message });
+})
