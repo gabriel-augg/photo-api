d7c6b87518f92761bccddcd0a02d145280532b20
Gabriel <gabrielgapseu@gmail.com>
2024-05-21 20:18:36 -0300

    Feat: add utility function for google route

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,12 +1,25 @@
-import { sign } from "jsonwebtoken";
+import { sign } from 'jsonwebtoken';
+import bcryptjs from 'bcryptjs';
+
 const secret: string = process.env.JWT_SECRET!;
 
 export const createToken = (id: string): string => {
     const token = sign({ id }, secret);
     return token;
-}
+};
 
 export const createExpirationDate = (): Date => {
     const expirationDate = new Date(Date.now() + 1000 * 60 * 60 * 24 * 7);
     return expirationDate;
-}
+};
+
+export const createPassword = (): string => {
+    const password = Math.random().toString(36).slice(-8) + Math.random().toString(36).slice(-8);
+    const hashedPassword = bcryptjs.hashSync(password, 10);
+    return hashedPassword;
+};
+
+export const createUsername = (name: string): string => {
+    const username = name.split(' ').join('').toLowerCase() + Math.floor(Math.random() * 10000).toString();
+    return username;
+};
