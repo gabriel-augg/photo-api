e0492014f0900f74b3c3547592f656d07fe7873b
Gabriel <gabrielgapseu@gmail.com>
2024-05-23 16:17:06 -0300

    Feat: add /GET /api/phtos and /api/photos/:id routes test

--- src/routes/__tests__/photo.route.test.ts
+++ src/routes/__tests__/photo.route.test.ts
@@ -25,6 +25,38 @@ beforeEach(async () => {
     token = cookie[0].split('access_token=')[1].split(';')[0];
 });
 
+describe('/GET /api/photos', () => {
+    it('should return 200 and an array of photos', async () => {
+        const response = await request(app).get('/api/photos');
+
+        expect(response.status).toBe(200);
+        expect(response.body).toBeInstanceOf(Array);
+    });
+});
+
+describe('/GET /api/photos/:id', () => {
+
+    it('should return 404 when photo is not found', async () => {
+        const nonExistentPhotoId = new mongoose.Types.ObjectId();
+        
+        const response = await request(app).get(`/api/photos/${nonExistentPhotoId}`);
+
+        expect(response.status).toBe(404);
+        expect(response.body.message).toBe('Photo not found');
+    });
+
+    it('should return 200 when photo is found', async () => {
+        const response1 = await request(app).post('/api/photos/create').set('Cookie', [`access_token=${token}`]).send({
+            image_url: 'https://www.example.com/image.jpg',
+        });
+
+        const response2 = await request(app).get(`/api/photos/${response1.body._id}`);
+
+        expect(response2.status).toBe(200);
+        expect(response2.body.image_url).toBe('https://www.example.com/image.jpg');
+    });
+});
+
 describe('/POST /api/photos/create', () => {
     it('should return 401 if no token is provided', async () => {
         const response = await request(app).post('/api/photos/create');
